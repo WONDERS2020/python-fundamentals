581a81dd77cf1b92e49374fcf9e21e1a0b3070fa
WONDERS2020 <henryuffia@gmail.com>
2025-02-22 13:48:51 -0800

    datatype

---
+++ module-1/2-datatype.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+""" A file that contains data types"""
+num = 5
+street = "Capital gateway"
+answer = False
+amount = 500.00
+
+print(type(num))
+print(type(street))
+print(type(answer))
+print(type(amount))

f6ae4c010d16d6ee3bae399d12ebd204101bb0b6
WONDERS2020 <henryuffia@gmail.com>
2025-03-17 02:51:16 -0700

    class

---
+++ module-2/10-convertupper.py
@@ -0,0 +1,8 @@
+#/usr/bin/python3
+name = input("Enter your name:")
+upper = name.upper()                 
+print(upper)
+
+"""a = input("Enter a Text:")
+lower = a.lower()                     
+print(lower)"""

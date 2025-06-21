ec0ff1185b008309cd842c3f84fcbef44cb29fa7
WONDERS2020 <henryuffia@gmail.com>
2025-06-21 10:53:58 +0100

    job

---
+++ module-7/0-open.py
@@ -0,0 +1,4 @@
+#/usr/bin/python3
+""" A program that open a file"""
+file = open("newfile.txt", "w")
+file.close()
---
+++ module-7/1-write.py
@@ -0,0 +1,5 @@
+#/usr/bin/python3
+""" A program that can write a file"""
+file = open("newfile.txt", "w")
+file.write("i climed the staircase")
+file.close()
